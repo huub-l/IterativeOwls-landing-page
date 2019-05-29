82842375ae17d02dc8e206a1027a0bf5fd493e9c
Hao Li <haoli2@andrew.cmu.edu>
2019-05-29 18:18:48 -0400

    Add back-to-top button

---
+++ assets/js/custom.js
@@ -0,0 +1,12 @@
+  // Back to top button
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn('slow');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+    }
+  });
+  $('.back-to-top').click(function(){
+    $('html, body').animate({scrollTop : 0},1000);
+    return false;
+  });
